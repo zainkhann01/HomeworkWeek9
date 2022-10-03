7d0c284c3ff3a5563139f95b56fd10fdb0140266
zainkhann01 <114729809+zainkhann01@users.noreply.github.com>
2022-10-03 13:07:45 +1100

    Add files via upload

---
+++ vegaLite_vis.js
@@ -0,0 +1,5 @@
+var vg_1 = "maps.json";
+
+vegaEmbed("#map", vg_1).then(function(result) {
+ // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
